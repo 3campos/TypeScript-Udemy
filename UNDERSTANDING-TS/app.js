9314b7669005248b5362e65a7a69b2c431a7a9b1
ecampos <ecampos.jr54@gmail.com>
2024-07-14 01:52:36 -0500

    add enum

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -2,20 +2,39 @@
 //     name: string;
 //     age: number;
 // } = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+//     //role is a tuple. Dictates the number of elements with specific types in an array.
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+var ADMIN = 0;
+var READ_ONLY = 1;
+var AUTHOR = 2;
 var person = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: ADMIN
 };
 // person.role.push('admin');
 // person.role[1]=10;
-person.role = [0, 'admin'];
+// person.role=[0, 'admin'];
 var favoriteActivities;
+favoriteActivities = ['Sports'];
 //string[], conveys that we want to return an array with a specific type, i.e., strings
 //use any[] to have a mixed types array
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === ADMIN) {
+    console.log('is admin');
 }
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -2,25 +2,38 @@
 //     name: string;
 //     age: number;
 // } = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-    //role is a tuple. Dictates the number of elements with specific types in an array.
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+//     //role is a tuple. Dictates the number of elements with specific types in an array.
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+const ADMIN = 0;
+const READ_ONLY =1;
+const AUTHOR=2;
+
+const person = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: ADMIN
 };
+
 
 // person.role.push('admin');
 // person.role[1]=10;
 
-person.role=[0, 'admin'];
+// person.role=[0, 'admin'];
 
 let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
 //string[], conveys that we want to return an array with a specific type, i.e., strings
 //use any[] to have a mixed types array
 
@@ -28,4 +41,8 @@ console.log(person.name)
 
 for(const hobby of person.hobbies) {
     console.log(hobby.toUpperCase())
+}
+
+if (person.role === ADMIN){
+    console.log('is admin');
 }
