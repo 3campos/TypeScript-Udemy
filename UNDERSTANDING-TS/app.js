77312ab25824ae094e7a9a4e19264e68d5e3047a
ecampos <ecampos.jr54@gmail.com>
2024-07-14 01:46:53 -0500

    add tuple to app.ts

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -1,5 +1,21 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
 var person = {
     name: 'Maximilian',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
-console.log(person);
+// person.role.push('admin');
+// person.role[1]=10;
+person.role = [0, 'admin'];
+var favoriteActivities;
+//string[], conveys that we want to return an array with a specific type, i.e., strings
+//use any[] to have a mixed types array
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -2,11 +2,23 @@
 //     name: string;
 //     age: number;
 // } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+    //role is a tuple. Dictates the number of elements with specific types in an array.
+} = {
     name: 'Maximilian',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+
+// person.role.push('admin');
+// person.role[1]=10;
+
+person.role=[0, 'admin'];
 
 let favoriteActivities: string[];
 //string[], conveys that we want to return an array with a specific type, i.e., strings
@@ -15,5 +27,5 @@ let favoriteActivities: string[];
 console.log(person.name)
 
 for(const hobby of person.hobbies) {
-    console.log(hobby)
+    console.log(hobby.toUpperCase())
 }
