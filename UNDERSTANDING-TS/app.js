e555e8f0eaaa386a45a343719334892bce7a0ba1
ecampos <ecampos.jr54@gmail.com>
2024-07-14 00:31:13 -0500

    bug fix: add numbers before string conversion

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -1,9 +1,9 @@
-function add(n1, n2, showResult) {
+function add(n1, n2, showResult, phrase) {
     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
         throw new Error('Incorrect input!');
     }
     if (showResult) {
-        console.log(n1 + n2);
+        console.log(phrase + n1 + n2);
     }
     else {
         return n1 + n2;
@@ -12,4 +12,5 @@ function add(n1, n2, showResult) {
 var number1 = 5;
 var number2 = 2.8;
 var printResult = true;
-add(number1, number2, printResult);
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
