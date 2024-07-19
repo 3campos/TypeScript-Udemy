7fa8f0f323aa80be6d5a4a45ef504a861eee90fc
ecampos <ecampos.jr54@gmail.com>
2024-07-19 00:00:59 -0500

    add a function type example

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -4,4 +4,14 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+//this function has a return type of "void", i.e., it doesn't return anything
 printResult(add(5, 12));
+function printResult2(num) {
+    console.log('Result: ' + num);
+    return;
+}
+//undefined is a valid type in typescript if you enter a return statement without returning an actual value
+printResult2(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -16,3 +16,11 @@ function printResult2(num: number): undefined{
 //undefined is a valid type in typescript if you enter a return statement without returning an actual value
 printResult2(add(5,12))
 
+let combineValues: (a: number, b: number) => number;
+//combineValues should accept any fn that takes 2 parameters, where each parameter is a number and where the function overall returns a number
+//the add function comports with the definition of combineValues (a:number, b: number) => number
+
+combineValues = add;
+// combineValues = 5;
+
+console.log(combineValues(8,8))
