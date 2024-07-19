7b9421202876b3d1f27f0750638c687ead94d965
ecampos <ecampos.jr54@gmail.com>
2024-07-18 23:47:57 -0500

    add undefined type example

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -8,3 +8,11 @@ function printResult(num: number){
 //this function has a return type of "void", i.e., it doesn't return anything
 
 printResult(add(5,12))
+
+function printResult2(num: number): undefined{
+    console.log('Result: ' + num)
+    return;
+}
+//undefined is a valid type in typescript if you enter a return statement without returning an actual value
+printResult2(add(5,12))
+
