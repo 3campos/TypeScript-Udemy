e1fe6291114420e817f653c3a727420f458e7406
ecampos <ecampos.jr54@gmail.com>
2024-07-21 00:23:58 -0500

    add tsconfig compilation exclusion and inclusion examples

--- UNDERSTANDING-TS/app.js
+++ UNDERSTANDING-TS/app.js
@@ -1,15 +1,3 @@
-var userInput;
-//we use unknown when we don't know what type the user will enter.
-var userName;
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-//the never type
-//never types are intended to never return anything
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-var result = generateError('An error occurred!', 500);
-console.log(result);
+"use strict";
+const userName = 'Emilio C.';
+console.log(userName);
--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,19 +1,3 @@
-let userInput: unknown;
-//we use unknown when we don't know what type the user will enter.
-let userName: string;
+const userName = 'Emilio C.';
 
-userInput = 5;
-userInput = 'Max';
-
-if(typeof userInput === 'string'){
-userName=userInput
-}
-
-//the never type
-//never types are intended to never return anything
-function generateError(message: string, code: number): never {
-    throw {message: message, errorCode: code};
-}
-
-const result = generateError('An error occurred!', 500);
-console.log(result);
+console.log(userName);
