7f7a619029bdcfe6ce458d0b8d186a82f22ade99
ecampos <ecampos.jr54@gmail.com>
2024-07-22 17:18:51 -0500

    add example with rest parameters included in function

--- UNDERSTANDING-TS/dist/app.js
+++ UNDERSTANDING-TS/dist/app.js
@@ -2,14 +2,6 @@
 const userName = 'Max';
 let age = 30;
 age = 29;
-const add = (a, b = 1) => a + b;
-console.log(add(2, 5));
-const printOutput = output => console.log(output);
-const button = document.querySelector('button');
-if (button) {
-    button.addEventListener('click', event => console.log(event));
-}
-printOutput(add(5));
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking', ...hobbies];
 activeHobbies.push(...hobbies);
@@ -18,4 +10,11 @@ const person = {
     age: 30
 };
 const copiedPerson = Object.assign({}, person);
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
 //# sourceMappingURL=app.js.map
--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -15,20 +15,20 @@ age = 29;
 
 // console.log(isOld)
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 //default arguments like "=1" for parameter "b", must be last in the parameter list.
 
-console.log(add(2, 5))
+// console.log(add(2, 5))
 
-const printOutput: (a: string | number ) => void = output => console.log(output);
+// const printOutput: (a: string | number ) => void = output => console.log(output);
 
-const button = document.querySelector('button')
+// const button = document.querySelector('button')
 
-if (button){
-    button.addEventListener('click', event => console.log(event))
-}
+// if (button){
+//     button.addEventListener('click', event => console.log(event))
+// }
 
-printOutput(add(5));
+// printOutput(add(5));
 
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking', ...hobbies]
@@ -45,3 +45,13 @@ const person = {
 
 const copiedPerson = { ...person };
 //the spread operator can also be used to add a list of key-value pairs from one object to another
+
+//rest parameters example. Rest parameters are used to accept an unlimited amount of arguments
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+}
+
+const addedNumbers = add(5, 10, 2, 3.7)
+console.log(addedNumbers)
