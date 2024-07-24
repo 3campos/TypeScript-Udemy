887755073aeef1635da25bde70a8211131cc3d42
ecampos <ecampos.jr54@gmail.com>
2024-07-23 23:44:51 -0500

    add class and instance example with a 'this' parameter

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -6,9 +6,17 @@ class Department {
     }
     //constructor is a reserved keyword
     //this is a function tied to this class and to any object you create from this class that will be executed when the object is created.
+    describe(this: Department){
+        console.log('Department' + this.name);
+        //what "this" in this.name is doing: when describe is executed, "this" refers to an instance that is based on the department class.
+    }
 }
 
 const accounting = new Department('Accounting');
 //this will create a JavaScript object
 
-console.log(accounting)
+accounting.describe()
+
+// const accountingCopy = {describe: accounting.describe}
+
+// accountingCopy.describe()
