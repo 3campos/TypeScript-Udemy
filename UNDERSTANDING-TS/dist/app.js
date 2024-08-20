8ce2f35e7e136e30d6e5e62263dbff76e2fb6bbe
ecampos <ecampos.jr54@gmail.com>
2024-08-20 14:21:08 -0500

    add dependencies to resolve MIME err & add api key

--- UNDERSTANDING-TS/dist/app.js
+++ UNDERSTANDING-TS/dist/app.js
@@ -1,9 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 const form = document.querySelector('form');
 const addressInput = document.getElementById('address');
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 function searchAddressHandler(event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
+    axios_1.default.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`).then(response => {
+        console.log(response);
+    })
+        .catch(err => (console.log(err)));
 }
 form === null || form === void 0 ? void 0 : form.addEventListener('submit', searchAddressHandler);
 //# sourceMappingURL=app.js.map
