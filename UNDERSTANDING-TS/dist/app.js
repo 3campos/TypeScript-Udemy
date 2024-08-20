281718739e2c2fe83881917f5044436c69733de1
ecampos <ecampos.jr54@gmail.com>
2024-08-19 23:27:24 -0500

    add searchAddressHandler function

--- UNDERSTANDING-TS/dist/app.js
+++ UNDERSTANDING-TS/dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
+const form = document.querySelector('form');
+const addressInput = document.getElementById('address');
+function searchAddressHandler(event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+}
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', searchAddressHandler);
 //# sourceMappingURL=app.js.map
