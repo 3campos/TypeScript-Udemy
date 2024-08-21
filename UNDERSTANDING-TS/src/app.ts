88d156fb3018f649db74db1022c99e7cf9e635a0
ecampos <ecampos.jr54@gmail.com>
2024-08-21 15:15:55 -0500

    add map search function using google maps API

--- UNDERSTANDING-TS/src/app.ts
+++ UNDERSTANDING-TS/src/app.ts
@@ -4,6 +4,14 @@ const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
+declare var google: any;
+
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`;
+script.async = true;
+script.defer = true;
+document.head.appendChild(script);
+
 type GoogleGeocodingResponse = {
     results: {
         geometry: {location: {lat: number; lng: number}}
@@ -22,6 +30,11 @@ axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode
         throw new Error('Could not fetch location!')
     }
 const coordinates = response.data.results[0].geometry.location;   
+const map = new google.maps.Map(document.getElementById("map"), {
+    center: coordinates,
+    zoom: 16,
+  });
+  new google.maps.Marker({position: coordinates, map: map});
 })
 .catch(err => {
     alert(err.message);
