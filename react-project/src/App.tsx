a155a777da498c88546c1bdb9c24a58a72d75510
ecampos <ecampos.jr54@gmail.com>
2024-08-22 13:21:48 -0500

    add react component and use interface to pass down props

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const App: React.FC = () => {
+import TodoList from './components/TodoList';
+
+const App: React.FunctionComponent = () => {
+  const todos = [{id: 't1', text: 'Finish the course'}];
   return (
     <div className="App">
-      
+      {/* A components that adds todos. */}
+      <TodoList items={todos}/>
     </div>
   );
 }
