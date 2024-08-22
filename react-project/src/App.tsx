fe246aa7bfa36b0631a4e648fc11529bc69619b2
ecampos <ecampos.jr54@gmail.com>
2024-08-22 14:59:59 -0500

    add todoAddHandler function to pass enteredText from NewTodo to App component

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 
-const App: React.FunctionComponent = () => {
+console.log(NewTodo)
+
+const App: React.FunctionComponent = props => {
   const todos = [{id: 't1', text: 'Finish the course'}];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text)
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler}/>
       <TodoList items={todos} />
     </div>
   );
--- react-project/src/components/NewTodo.tsx
+++ react-project/src/components/NewTodo.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const NewTodo: React.FC = () => {
-    return <form></form>
+type NewTodoProps = {
+    onAddTodo: (todoText: string) => void;
+}
+
+const NewTodo: React.FC<NewTodoProps> = props => {
+
+    const textInputRef = useRef<HTMLInputElement>(null);
+
+    const todoSubmitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        //the default action being prevented is the sending of a request to the server
+        const enteredText = textInputRef.current!.value as any;
+        props.onAddTodo(enteredText)
+        // console.log(enteredText)
+    };
+    //the todoSubmitHandler function should fire whenever the below form is submitted, so we can bind the form to the todoSubmitHandler by adding "onSubmit" like so: <form onSubmit={todoSubmitHandler}> ... </form>
+
+    return (
+    <form onSubmit={todoSubmitHandler}>
+        <div>
+            <label htmlFor="todo=text">Todo Text</label>
+            <input type="text" id="todo-text" ref={textInputRef}/>
+        </div>
+        <button type="submit">ADD TODO</button>
+    </form>)
 };
 
 export default NewTodo;
