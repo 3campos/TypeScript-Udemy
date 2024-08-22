a3e57d19d5c3a77a4e5ed27498c22a1a70956f08
ecampos <ecampos.jr54@gmail.com>
2024-08-22 13:24:03 -0500

    add NewTodo component

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 const App: React.FunctionComponent = () => {
   const todos = [{id: 't1', text: 'Finish the course'}];
   return (
     <div className="App">
-      {/* A components that adds todos. */}
-      <TodoList items={todos}/>
+      <NewTodo />
+      <TodoList items={todos} />
     </div>
   );
 }
